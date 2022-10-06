a4ff3cc62b4c30f0114cfc4b258bf2ff18854d13
AlexxSerban <109248856+AlexxSerban@users.noreply.github.com>
2022-10-06 21:57:22 +0300

    Initial Commit

---
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Alex Serban on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
