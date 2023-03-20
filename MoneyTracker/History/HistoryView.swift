2922da38abbb162cd074ccc6f23b4e1a191814be
AlexxSerban <109248856+AlexxSerban@users.noreply.github.com>
2023-03-20 16:35:57 +0200

    Update HistoryView.swift

--- MoneyTracker/History/HistoryView.swift
+++ MoneyTracker/History/HistoryView.swift
@@ -153,7 +153,6 @@ struct HistoryView: View {
                                         .font(.system(size: 16, design: .serif))
                                         .foregroundColor(Color("Text"))
                                     
-                                    
                                 }
                             }
                     }
